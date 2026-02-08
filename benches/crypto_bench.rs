b6d6bfb9bff7c3e240c40d3bb0c945db0ac5cbcd
Rob Dekovich <rob@altoal.com>
2026-02-08 04:12:16 +0000

    fix: rename burrow→dugout in benchmark

--- benches/crypto_bench.rs
+++ benches/crypto_bench.rs
@@ -1,5 +1,5 @@
 use age::x25519;
-use burrow::bench::{Age, Cipher};
+use dugout::bench::{Age, Cipher};
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use std::time::Duration;
 
