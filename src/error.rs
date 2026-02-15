d6a51d7c4d01c66345c05fd73f7b2493e9470801
Robert Dekovich <rob@altoal.com>
2026-02-14 20:23:10 -0600

    feat(error): add MultipleVaults error type

--- src/error.rs
+++ src/error.rs
@@ -22,6 +22,9 @@ pub enum ConfigError {
     #[error("identity does not have access to this vault")]
     AccessDenied,
 
+    #[error("multiple vaults found\n\n{vaults}\n\nspecify which vault to use:\n\n  dugout -v <name> <command>\n\nor set DUGOUT_VAULT environment variable:\n\n  export DUGOUT_VAULT=<name>")]
+    MultipleVaults { vaults: String },
+
     #[error("missing required field: {field}")]
     MissingField { field: &'static str },
 
